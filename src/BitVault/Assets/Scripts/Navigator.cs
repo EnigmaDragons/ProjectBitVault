50ff709dcb9cf4624a209b7e583ff6fd79bffb94
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-01 18:56:58 -0700

    Added Logo Scene. Closes #5. Closes #8

--- src/BitVault/Assets/Scripts/Navigator.cs
+++ src/BitVault/Assets/Scripts/Navigator.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public sealed class Navigator : ScriptableObject
 {
+    public void NavigateToMainMenu() => NavigateTo("MainMenu");
+    
     private void NavigateTo(string name)
     {
         Debug.Log($"Navigating to {name}");
