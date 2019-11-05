be370ed17df83fc40afeef4fda40186a670eec1a
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-04 17:52:47 -0700

    Added basic Selection Piece indicator. Closes #28

---
+++ src/BitVault/Assets/Scripts/UI/SelectedTilePresenter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public sealed class SelectedTilePresenter : MonoBehaviour
+{
+    [SerializeField] private GameObject selectionIndicator;
+
+    private void OnEnable()
+    {
+        Message.Subscribe<PieceSelected>(p => Show(p.Piece), this);
+        Message.Subscribe<PieceDeselected>(_ => Hide(), this);
+    }
+
+    private void Hide()
+    {
+        selectionIndicator.transform.SetParent(transform);
+        selectionIndicator.SetActive(false);
+    }
+
+    private void Show(GameObject p)
+    {
+        selectionIndicator.transform.position = p.transform.position + new Vector3(0, 0, selectionIndicator.transform.position.z);
+        selectionIndicator.transform.SetParent(p.transform);
+        selectionIndicator.SetActive(true);
+    }
+}
