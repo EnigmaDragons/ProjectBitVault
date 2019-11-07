afb20a7e05316c3875e53a77b658e8f57e0ed381
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-06 22:35:53 -0700

    Gain Star On Level Complete. Closes #46

--- src/BitVault/Assets/Scripts/NavigateToLevelSelectOnLevelComplete.cs
+++ src/BitVault/Assets/Scripts/NavigateToLevelSelectOnLevelComplete.cs
@@ -1,9 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class NavigateToLevelSelectOnLevelComplete : OnMessage<LevelCompleted>
 {
     [SerializeField] private Navigator navigator;
 
-    protected override void Execute(LevelCompleted msg) => navigator.NavigateToLevelSelect();
+    protected override void Execute(LevelCompleted msg) => StartCoroutine(NavigateAfterDelay());
+
+    private IEnumerator NavigateAfterDelay()
+    {
+        yield return new WaitForSeconds(1);
+        navigator.NavigateToLevelSelect();
+    }
 }
   
