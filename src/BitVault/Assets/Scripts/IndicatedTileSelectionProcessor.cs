7e15737dc713716e2cb0c6d8c3fbe729f53a8039
SilasReinagel <silas.reinagel@gmail.com>
2019-11-07 19:27:37 -0700

    Selects Any Selectable on Click. Closes #66

--- src/BitVault/Assets/Scripts/IndicatedTileSelectionProcessor.cs
+++ src/BitVault/Assets/Scripts/IndicatedTileSelectionProcessor.cs
@@ -3,13 +3,8 @@ using UnityEngine;
 public sealed class IndicatedTileSelectionProcessor : OnMessage<TileIndicated>
 {
     [SerializeField] private CurrentLevelMap map;
-    [SerializeField] private CurrentSelectedPiece piece;
 
-    protected override void Execute(TileIndicated msg)
-    {
-        if (piece.Selected.IsPresent) return;
-
-        map.GetSelectable(msg.Tile)
+    protected override void Execute(TileIndicated msg) 
+        => map.GetSelectable(msg.Tile)
             .IfPresent(o => Message.Publish(new PieceSelected(o)));
-    }
 }
