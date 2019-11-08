1df860370d054a3c22b18d33ef581548709d6add
SilasReinagel <silas.reinagel@gmail.com>
2019-11-07 19:22:01 -0700

    Can Activate Activatables. Closes #41

--- src/BitVault/Assets/Scripts/GameObjects/DestroyIfJumped.cs
+++ src/BitVault/Assets/Scripts/GameObjects/DestroyIfJumped.cs
@@ -7,9 +7,6 @@ public class DestroyIfJumped : OnMessage<PieceMoved>
     protected override void Execute(PieceMoved msg)
     {
         if (msg.From.IsAdjacentTo(new TilePoint(gameObject)) && msg.To.IsAdjacentTo(new TilePoint(gameObject)) && (msg.To.X == msg.From.X || msg.To.Y == msg.From.Y))
-        {
-            map.Remove(gameObject);
-            Destroy(gameObject);
-        }
+            Message.Publish(new ObjectDestroyed(gameObject));
     }
 }
