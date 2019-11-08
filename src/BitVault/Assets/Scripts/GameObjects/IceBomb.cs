7ead93722403379ea29d6c280185965f7bdb71d1
noah reinagel <noah.r9@gmail.com>
2019-11-07 22:59:12 -0700

    made ice bomb

---
+++ src/BitVault/Assets/Scripts/GameObjects/IceBomb.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class IceBomb : OnMessage<PieceMoved>
+{
+    protected override void Execute(PieceMoved msg)
+    {
+        if (msg.To.DistanceFrom(new TilePoint(gameObject)) == 1)
+        {
+            Message.Publish(new ObjectDestroyed(gameObject));
+            Message.Publish(new ObjectDestroyed(msg.Piece));
+        }
+    }
+}
