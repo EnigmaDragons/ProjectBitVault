a18fb085918fbf8c95bb73801d481819649671b7
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-08 19:40:59 -0700

    Added Double Jump Prototype Level. Closes #75

--- src/BitVault/Assets/Scripts/GameObjects/DestroyIfJumped.cs
+++ src/BitVault/Assets/Scripts/GameObjects/DestroyIfJumped.cs
@@ -1,9 +1,6 @@
-﻿using UnityEngine;
-
+﻿
 public class DestroyIfJumped : OnMessage<PieceMoved>
 {
-    [SerializeField] private CurrentLevelMap map;
-
     protected override void Execute(PieceMoved msg)
     {
         if (msg.From.IsAdjacentTo(new TilePoint(gameObject)) && msg.To.IsAdjacentTo(new TilePoint(gameObject)) && (msg.To.X == msg.From.X || msg.To.Y == msg.From.Y))
