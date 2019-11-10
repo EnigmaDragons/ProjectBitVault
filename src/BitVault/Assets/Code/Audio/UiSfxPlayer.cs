c199a49760bc05b18771fc775fd0ebe27b5c2f1d
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-10 15:10:02 -0700

    Added UiSoundSource. Closes #85

---
+++ src/BitVault/Assets/Code/Audio/UiSfxPlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public sealed class UiSfxPlayer : ScriptableObject
+{
+    [SerializeField, DTValidator.Optional] private AudioSource source;
+
+    public void Init(AudioSource src) => source = src;
+    public void InitIfNeeded(AudioSource src) => source.IfNull(() => Init(src));
+    public void Play(AudioClip c) => source.PlayOneShot(c);
+}
