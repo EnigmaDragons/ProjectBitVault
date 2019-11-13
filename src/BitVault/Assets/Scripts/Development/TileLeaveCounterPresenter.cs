23f8c84cf4d03bc854cf6b262b1d2070e376d8b4
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-12 17:54:23 -0700

    Created Puzzle 15. Closes #116

---
+++ src/BitVault/Assets/Scripts/Development/TileLeaveCounterPresenter.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TileLeaveCounterPresenter : OnMessage<PieceMoved>
+{
+    [SerializeField] private TextMeshPro text;
+    [SerializeField] private BoolReference isEnabled;
+
+    private int _count = 0;
+    private TilePoint _pos;
+
+    private void Awake()
+    {
+        _pos = new TilePoint(gameObject);
+        if (!Debug.isDebugBuild || !isEnabled)
+            Destroy(gameObject);
+    }
+
+    protected override void Execute(PieceMoved msg)
+    {
+        if (!msg.From.Equals(_pos)) return;
+        
+        _count++;
+        text.text = _count.ToString();
+    }
+}
