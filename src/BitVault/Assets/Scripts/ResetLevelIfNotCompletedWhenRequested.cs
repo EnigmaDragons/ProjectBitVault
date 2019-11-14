5eca563dac9bd31caee3dfae41a679780fb61dab
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-13 22:54:33 -0700

    Setup foundation for indirecting Level Reset Requests.

---
+++ src/BitVault/Assets/Scripts/ResetLevelIfNotCompletedWhenRequested.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public sealed class ResetLevelIfNotCompletedWhenRequested : MonoBehaviour
+{
+    [SerializeField] private GameState state;
+
+    private bool _isCompleted;
+
+    private void OnEnable()
+    {
+        Message.Subscribe<LevelResetRequested>(_ => Reset(), this);
+        Message.Subscribe<LevelCompleted>(_ => _isCompleted = false, this);
+    }
+
+    private void OnDisable() => Message.Unsubscribe(this);
+
+    private void Reset()
+    {
+        if (!_isCompleted)
+            state.InitLevel();
+    }
+}
