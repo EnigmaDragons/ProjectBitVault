1e179b718549aab405d52175bf1a830c118cf7d4
SilasReinagel <silas.reinagel@gmail.com>
2019-11-18 23:34:53 -0700

    Added Root Access Sound. Closes #173

---
+++ src/BitVault/Assets/Scripts/Sounds/PlayLevelCompleteVictorySound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayLevelCompleteVictorySound : OnMessage<LevelCompleted>
+{
+    [SerializeField] private UiSfxPlayer player;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private FloatReference volume = new FloatReference(0.5f);
+    
+    protected override void Execute(LevelCompleted msg) => player.Play(clip, volume);
+}
