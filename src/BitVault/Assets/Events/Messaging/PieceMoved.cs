a949c4227af29a3445c5308ad679cbd9468d936e
noah reinagel <noah.r9@gmail.com>
2019-11-19 20:06:33 -0700

    esigned new piece and updated previous pieces

--- src/BitVault/Assets/Events/Messaging/PieceMoved.cs
+++ src/BitVault/Assets/Events/Messaging/PieceMoved.cs
@@ -5,6 +5,7 @@ public sealed class PieceMoved
     public GameObject Piece { get; }
     public TilePoint From { get; }
     public TilePoint To { get; }
+    public TilePoint Delta => To - From;
 
     public PieceMoved(GameObject obj, TilePoint from, TilePoint to)
     {
