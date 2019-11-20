ea33762c84e8387f41c6364f576a28a16873c143
noah reinagel <noah.r9@gmail.com>
2019-11-19 23:59:24 -0700

    Added mini datacube that appears if there is only one thing left fixed bug with level Z3 an open port with wrong victory condition made it more discoverable in the future by removing that victory condition improved drag functionality slightly

--- src/BitVault/Assets/Scripts/Rules/AllSelectablesMustBeAdjacentToBitVault.cs
+++
@@ -1,7 +0,0 @@
-using System.Linq;
-
-public sealed class AllSelectablesMustBeAdjacentToBitVault : VictoryCondition
-{
-    public override bool HasCompletedLevel(CurrentLevelMap map)
-        => map.Selectables.All(s => new TilePoint(s).IsAdjacentTo(map.BitVaultLocation));
-}
