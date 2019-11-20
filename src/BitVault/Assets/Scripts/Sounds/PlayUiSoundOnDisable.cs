82210b095b4cfa130c22eb5ed3f14d0ac1f7431f
SilasReinagel <silas.reinagel@gmail.com>
2019-11-19 19:06:47 -0700

    Game Menu Open/Closes sounds. Closes #210. Closes #211

---
+++ src/BitVault/Assets/Scripts/Sounds/PlayUiSoundOnDisable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayUiSoundOnDisable : MonoBehaviour
+{
+    [SerializeField] private UiSfxPlayer player;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private FloatReference volume = new FloatReference(0.5f);
+
+    private void OnDisable() => player.Play(clip, volume);
+}
---
+++ src/BitVault/Assets/Scripts/Sounds/PlayUiSoundOnEnable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayUiSoundOnEnable : MonoBehaviour
+{
+    [SerializeField] private UiSfxPlayer player;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private FloatReference volume = new FloatReference(0.5f);
+
+    private void OnEnable() => player.Play(clip, volume);
+}
