a50d9eceb75c21c8d44d6d3807c3c48a136a48b4
noah reinagel <noah.r9@gmail.com>
2019-11-22 04:00:31 -0700

    put in story 2 and fixed the tutorials

--- src/BitVault/Assets/Scripts/GameObjects/FallingTile.cs
+++ src/BitVault/Assets/Scripts/GameObjects/FallingTile.cs
@@ -10,6 +10,7 @@ public class FallingTile : OnMessage<PieceMoved>
     [SerializeField] private LockBoolVariable gameInputActive;
     [SerializeField] private float _lossDelay;
     [SerializeField] private CurrentLevelMap map;
+    [SerializeField] private BoolReference tutorialActive;
 
     private bool _isDangerous = false;
 
@@ -18,19 +19,23 @@ public class FallingTile : OnMessage<PieceMoved>
         if (msg.From.Equals(new TilePoint(gameObject)) && !_isDangerous)
         {
             _isDangerous = true;
-            renderer.material = map.BitVaultLocation.IsAdjacentTo(new TilePoint(gameObject)) ? dangerousGoalMaterial : dangerousMaterial;
+            if (tutorialActive)
+                renderer.material = dangerousMaterial;
+            else
+                renderer.material = map.BitVaultLocation.IsAdjacentTo(new TilePoint(gameObject)) ? dangerousGoalMaterial : dangerousMaterial;
         }
         else if (msg.To.Equals(new TilePoint(gameObject)) && _isDangerous)
         {
             gameInputActive.Lock(gameObject);
-            StartCoroutine(DelayedLoss());
+            StartCoroutine(DelayedLoss(tutorialActive.Value));
         }
     }
 
-    private IEnumerator DelayedLoss()
+    private IEnumerator DelayedLoss(bool isTutorialActive)
     {
         yield return new WaitForSeconds(_lossDelay);
         gameInputActive.Unlock(gameObject);
-        gameState.InitLevel();
+        if (!isTutorialActive)
+            gameState.InitLevel();
     }
 }
