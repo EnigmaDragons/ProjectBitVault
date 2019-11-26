ef78ed6a3e1663694de093cdd6a0fc352ff4e826
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-25 22:56:00 -0700

    Set Company Colors. Closes #278

---
+++ src/BitVault/Assets/Scripts/UI/LevelSelect/ZoneBorderColor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoneBorderColor : GameEventTrigger
+{
+    [SerializeField] private CurrentZone zone;
+    [SerializeField] private Image image;
+
+    protected override GameEvent Trigger => zone.OnCurrentZoneChanged;
+    protected override void Execute()
+    {
+        Debug.Log("Updated Color");
+        image.color = zone.Zone.BackgroundColor;
+    }
+}
