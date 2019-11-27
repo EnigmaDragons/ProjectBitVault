b2727f7940f178a68de1ea0696938a186849ea83
Silas Reinagel <silas.reinagel@carvana.com>
2019-11-27 13:55:38 -0700

    Can advance Dialogue using Keyboard controls. Closes #295

--- src/BitVault/Assets/Scripts/Inputs/OnKeyPress.cs
+++ src/BitVault/Assets/Scripts/Inputs/OnKeyPress.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,10 +6,11 @@ public sealed class OnKeyPress : MonoBehaviour
 {
     [SerializeField] private KeyCode key;
     [SerializeField] private UnityEvent action;
+    [SerializeField] private KeyCode[] alternateKeys;
 
     private void Update()
     {
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(key) || alternateKeys.Any(Input.GetKeyDown))
             action.Invoke();
     }
 }
