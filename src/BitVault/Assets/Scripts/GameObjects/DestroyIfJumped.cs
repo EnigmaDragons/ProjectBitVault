a87f73cdd00e022b337903d9a390362c5d1a0bdb
noah reinagel <noah.r9@gmail.com>
2019-11-28 15:37:19 -0700

    changed the shape of registry entries and fixed a bug where registry entries would not destroy properly if it got jumped after an odd number of its movements

--- src/BitVault/Assets/Scripts/GameObjects/DestroyIfJumped.cs
+++ src/BitVault/Assets/Scripts/GameObjects/DestroyIfJumped.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class DestroyIfJumped : OnMessage<PieceMoved>
 {
-    [SerializeField] private Renderer renderer;
+    [SerializeField] private Renderer[] renderers;
     [SerializeField] private Texture deathMask;
     [SerializeField] private float secondsTilDeath = 1;
 
@@ -23,6 +23,11 @@ public class DestroyIfJumped : OnMessage<PieceMoved>
     {
         _isDying = true;
         _t = 0;
+        renderers.ForEach(SetupForDeath);
+    }
+
+    private void SetupForDeath(Renderer renderer)
+    {
         renderer.material.SetTexture("_DisplacementMask", deathMask);
         renderer.material.SetFloat("_DefaultShrink", 0);
         renderer.material.SetFloat("_NormalPush", 0);
@@ -34,9 +39,14 @@ public class DestroyIfJumped : OnMessage<PieceMoved>
         if (!_isDying)
             return;
         _t = Math.Min(1, _t + Time.deltaTime / secondsTilDeath);
-        renderer.material.SetFloat("_DefaultShrink", _t);
-        renderer.material.SetFloat("_NormalPush", _t);
+        renderers.ForEach(ApproachDeath);
         if (_t == 1)
             gameObject.SetActive(false);
+    }
+
+    private void ApproachDeath(Renderer renderer)
+    {
+        renderer.material.SetFloat("_DefaultShrink", _t);
+        renderer.material.SetFloat("_NormalPush", _t);
     }
 }
