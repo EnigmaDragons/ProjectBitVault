e1719154fecb5c5726b0dc63529418797a602a4a
Silas Reinagel <silas.reinagel@carvana.com>
2019-12-08 15:54:04 -0700

    Fixed In Game Menu access bugs. Closes #336. Closes #337

--- src/BitVault/Assets/Scripts/UI/ToggleTarget.cs
+++ src/BitVault/Assets/Scripts/UI/ToggleTarget.cs
@@ -5,6 +5,22 @@ public sealed class ToggleTarget : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private GameObject[] additionalTargets;
 
-    public void Toggle() => target.Concat(additionalTargets).ForEach(t => t.SetActive(!t.activeSelf));
+    private bool _canToggle = true;
+    
+    private void OnEnable()
+    {
+        Message.Subscribe<LevelCompleted>(_ => _canToggle = false, this);
+    }
+
+    private void OnDisable()
+    {
+        Message.Unsubscribe(this);
+    }
+
+    public void Toggle()
+    {
+        if (_canToggle)
+            target.Concat(additionalTargets).ForEach(t => t.SetActive(!t.activeSelf));
+    }
 }
 
