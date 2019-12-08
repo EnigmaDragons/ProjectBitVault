a33b28dffc4cbc2fdc8e60e6305f882fc7228335
Silas Reinagel <silas.reinagel@carvana.com>
2019-12-07 20:39:41 -0700

    In-Game Menu is more visible. Closes #334

--- src/BitVault/Assets/Scripts/UI/ToggleTarget.cs
+++ src/BitVault/Assets/Scripts/UI/ToggleTarget.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public sealed class ToggleTarget : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private GameObject[] additionalTargets;
 
-    public void Toggle() => target.SetActive(!target.activeSelf);
+    public void Toggle() => target.Concat(additionalTargets).ForEach(t => t.SetActive(!t.activeSelf));
 }
 
