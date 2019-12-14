a79f40d4c53cfb2ff0a7d4cba91286288dc2ea99
Silas Reinagel <silas.reinagel@carvana.com>
2019-12-13 17:59:06 -0700

    Read Cached PlayerPrefs. Closes #351

--- src/BitVault/Assets/Scripts/SaveStorage.cs
+++ src/BitVault/Assets/Scripts/SaveStorage.cs
@@ -8,39 +8,35 @@ public class SaveStorage : ScriptableObject
     private const string _versionKey = "Version";
     private const string _version = "0.1";
     private const string _showMovementHints = "ShowMovementHints";
-
-    private void Awake() => PlayerPrefs.SetString(_versionKey, _version);
-
+    
+    private PlayerPrefsKeyValueStore _store = new PlayerPrefsKeyValueStore();
+    
+    public void Init()
+    {
+        if (_store == null)
+            _store = new PlayerPrefsKeyValueStore();
+        _store.Put(_versionKey, _version);
+    }
+    
+    private string StarsKey(GameLevel level) => $"{level.Name}Stars";
+    public int GetStars(GameLevel level) => _store.GetOrDefault(StarsKey(level), 0);
     public void SaveStars(GameLevel level, int stars)
     {
         var key = StarsKey(level);
         var previousStars = GetStars(level);
         if (previousStars < stars)
         {
-            PlayerPrefs.SetInt(key, stars);
+            _store.Put(key, stars);
             AddToTotalStars(stars - previousStars);
         }
     }
-
-    public int GetStars(GameLevel level)
-    {
-        var key = StarsKey(level);
-        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetInt(key) : 0;
-    }
-
-    public int GetTotalStars() => PlayerPrefs.HasKey(_totalStarsKey) ? PlayerPrefs.GetInt(_totalStarsKey) : 0;
     
-    public void SaveZone(int zone) => PlayerPrefs.SetInt(_zoneKey, zone);
+    public int GetZone() => _store.GetOrDefault(_zoneKey, 0);
+    public void SaveZone(int zone) => _store.Put(_zoneKey, zone);
 
-    public int GetZone() => PlayerPrefs.HasKey(_zoneKey) ? PlayerPrefs.GetInt(_zoneKey) : 0;
+    public int GetTotalStars() => _store.GetOrDefault(_totalStarsKey, 0);
+    private void AddToTotalStars(int addedStars) => _store.Update(_totalStarsKey, 0, existing => existing + addedStars);
 
-    private void AddToTotalStars(int addedStars)
-    {
-        PlayerPrefs.SetInt(_totalStarsKey, addedStars + (PlayerPrefs.HasKey(_totalStarsKey) ? PlayerPrefs.GetInt(_totalStarsKey) : 0));
-    }
-
-    private string StarsKey(GameLevel level) => $"{level.Name}Stars";
-
-    public bool GetShowMovementHints() => PlayerPrefs.HasKey(_showMovementHints) ? PlayerPrefs.GetInt(_showMovementHints) > 0 : true;
-    public void SetShowMovementHints(bool active) => PlayerPrefs.SetInt(_showMovementHints, active ? 1 : 0);
+    public bool GetShowMovementHints() => _store.GetOrDefault(_showMovementHints, true);
+    public void SetShowMovementHints(bool active) => _store.Put(_showMovementHints, active);
 }
