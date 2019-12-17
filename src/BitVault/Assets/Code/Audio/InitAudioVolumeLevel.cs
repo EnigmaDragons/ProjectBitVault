0a2dcdf4a018062a50870a33f8786ee4ca510019
Silas Reinagel <silas.reinagel@carvana.com>
2019-12-17 14:59:29 -0700

    Added basic animation to Logo. Closes #376

--- src/BitVault/Assets/Code/Audio/InitAudioVolumeLevel.cs
+++ src/BitVault/Assets/Code/Audio/InitAudioVolumeLevel.cs
@@ -5,10 +5,14 @@ public sealed class InitAudioVolumeLevel : MonoBehaviour
 {
     [SerializeField] private AudioMixer mixer;
     [SerializeField] private string valueName = "MusicVolume";
+    [SerializeField] private AudioSource player;
+    [SerializeField] private AudioClip demoSound;
     
     private void Awake()
     {
         var volume = PlayerPrefs.GetFloat(valueName, 0.5f);
         mixer.SetFloat(valueName, Mathf.Log10(volume) * 20);
+        if (player != null && demoSound != null)
+            player.PlayOneShot(demoSound, 1f);
     }
 }
