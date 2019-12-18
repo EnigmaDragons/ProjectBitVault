51312850e2ac3149e30ce651d4eaa5fe84d0f35b
noah reinagel <noah.r9@gmail.com>
2019-12-18 15:04:34 -0700

    Changed Data Cube from last subroutine appearence

---
+++ src/BitVault/Assets/Scripts/Rules/SpawnDataCubeIfOnlyOneSubroutine.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SpawnDataCubeIfOnlyOneSubroutine : OnMessage<LevelStateChanged>
+{
+    [SerializeField] private GameObject dataCube;
+    [SerializeField] private CurrentLevelMap map;
+
+    private bool _spawned;
+
+    protected override void Execute(LevelStateChanged msg)
+    {
+        if (map.NumOfJumpables == 1 && !_spawned)
+        {
+            var heroTile = new TilePoint(map.Hero.gameObject);
+            if (map.IsJumpable(new TilePoint(heroTile.X - 1, heroTile.Y)) && map.IsWalkable(new TilePoint(heroTile.X - 2, heroTile.Y)))
+                SpawnDataCube(new TilePoint(heroTile.X - 2, heroTile.Y));
+            if (map.IsJumpable(new TilePoint(heroTile.X + 1, heroTile.Y)) && map.IsWalkable(new TilePoint(heroTile.X + 2, heroTile.Y)))
+                SpawnDataCube(new TilePoint(heroTile.X + 2, heroTile.Y));
+            if (map.IsJumpable(new TilePoint(heroTile.X, heroTile.Y - 1)) && map.IsWalkable(new TilePoint(heroTile.X, heroTile.Y - 2)))
+                SpawnDataCube(new TilePoint(heroTile.X, heroTile.Y - 2));
+            if (map.IsJumpable(new TilePoint(heroTile.X, heroTile.Y + 1)) && map.IsWalkable(new TilePoint(heroTile.X, heroTile.Y + 2)))
+                SpawnDataCube(new TilePoint(heroTile.X, heroTile.Y + 2));
+        }
+    }
+
+    private void SpawnDataCube(TilePoint position)
+    {
+        var cube = Instantiate(dataCube);
+        cube.transform.localPosition = new Vector3(position.X, position.Y, cube.transform.localPosition.z);
+        _spawned = true;
+    }
+}
