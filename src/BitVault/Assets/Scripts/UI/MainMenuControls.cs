f4a9ddd8ac372fb7ecf972c858b121db5fc9c310
noah reinagel <noah.r9@gmail.com>
2020-01-11 10:33:19 -0700

    fixed bug with main menu controls improved credits

--- src/BitVault/Assets/Scripts/UI/MainMenuControls.cs
+++ src/BitVault/Assets/Scripts/UI/MainMenuControls.cs
@@ -6,13 +6,9 @@ public class MainMenuControls : MonoBehaviour
     [SerializeField] private Navigator navigator;
     [SerializeField] private Button newGameButton;
     [SerializeField] private Button continueButton;
-    [SerializeField] private Button tutorialButton;
-    [SerializeField] private Button storyArchiveButton;
     [SerializeField] private Button creditsButton;
     [SerializeField] private Button exitButton;
     [SerializeField] private GameObject exitButtonObject;
-    [SerializeField] private CurrentTutorial tutorial;
-    [SerializeField] private Dialogue masterTutorial;
     [SerializeField] private SaveStorage saveStorage;
 
     private void Awake()
@@ -21,12 +17,6 @@ public class MainMenuControls : MonoBehaviour
             exitButtonObject.SetActive(false);
         continueButton.interactable = saveStorage.HasChosenGender();
         continueButton.onClick.AddListener(() => navigator.NavigateToLevelSelect());
-        storyArchiveButton.onClick.AddListener(() => navigator.NavigateToArchive());
-        tutorialButton.onClick.AddListener(() =>
-        {
-            tutorial.Set(masterTutorial);
-            navigator.NavigateToTutorial();
-        });
         creditsButton.onClick.AddListener(() => navigator.NavigateToCredits());
         exitButton.onClick.AddListener(Application.Quit);
     }
--- src/BitVault/Assets/Scripts/UI/RandomSpawner.cs
+++ src/BitVault/Assets/Scripts/UI/RandomSpawner.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
 {
@@ -7,13 +9,26 @@ public class RandomSpawner : MonoBehaviour
     [SerializeField] private int maxSpawns;
     [SerializeField] private Vector3 minBounds;
     [SerializeField] private Vector3 maxBounds;
+    [SerializeField] private List<Renderer> toAvoidSpawningIn;
+
 
     public void Start()
     {
         for (var i = 0; i < Rng.Int(minSpawns, maxSpawns + 1); i++)
         {
             var spawn = Instantiate(prefab, transform);
-            spawn.transform.localPosition = new Vector3(Rng.Int((int)minBounds.x, (int)maxBounds.x), Rng.Int((int)minBounds.y, (int)maxBounds.y), Rng.Int((int)minBounds.z, (int)maxBounds.z));
+            spawn.transform.localPosition = GetRandomValidLocation();
         }
+    }
+
+    private Vector3 GetRandomValidLocation()
+    {
+        for (var i = 0; i < 9999; i++)
+        {
+            var location = new Vector3(Rng.Int((int)minBounds.x, (int)maxBounds.x), Rng.Int((int)minBounds.y, (int)maxBounds.y), Rng.Int((int)minBounds.z, (int)maxBounds.z));
+            if (toAvoidSpawningIn.All(x => !x.bounds.Contains(location)))
+                return location;
+        }
+        return Vector3.zero;
     }
 }
