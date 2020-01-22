aaa86181d8c8ed3bf6b406a4560e0f2cd2e09aaa
Silas Reinagel <silas.reinagel@carvana.com>
2020-01-21 17:12:04 -0700

    Started setting up Beta mode. Closes #494. Closes #496

--- src/BitVault/Assets/Scripts/UI/DisplayVersionNumberText.cs
+++ src/BitVault/Assets/Scripts/UI/DisplayVersionNumberText.cs
@@ -5,7 +5,9 @@ public sealed class DisplayVersionNumberText : MonoBehaviour
 {
     [SerializeField] private string prefix;
     [SerializeField] private StringReference value;
+    [SerializeField] private BoolReference betaMode;
     [SerializeField] private TextMeshProUGUI text;
 
-    private void Start() => text.text = $"{prefix}{value}";
+    private string Mode => betaMode ? "Beta" : "";
+    private void Update() => text.text = $"{prefix}{value} {Mode}";
 }
