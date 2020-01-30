7d781b7fbffbe8efa995cd513b7759d392cb94df
noah reinagel <noah.r9@gmail.com>
2020-01-30 15:10:30 -0700

    added new bool for the hint feature

---
+++ src/BitVault/Assets/Scripts/UI/SetActiveBasedOnBool.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class SetActiveBasedOnBool : MonoBehaviour
+{
+    [SerializeField] private BoolReference boolRef;
+
+    private void Start() => gameObject.SetActive(boolRef.Value);
+}
