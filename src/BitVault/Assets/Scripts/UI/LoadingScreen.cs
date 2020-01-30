400a865418367ca40766219d10a0022ba9dad96e
Silas Reinagel <silas.reinagel@carvana.com>
2020-01-29 19:05:16 -0700

    Spiffed up Loading Screen a little.

--- src/BitVault/Assets/Scripts/UI/LoadingScreen.cs
+++ src/BitVault/Assets/Scripts/UI/LoadingScreen.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
@@ -10,17 +13,23 @@ public class LoadingScreen : MonoBehaviour
     [SerializeField] private RectTransform barFillRectTransform;
     [SerializeField] private TextMeshProUGUI percentLoadedText;
     [SerializeField] private float timeBeforeShowing;
+    [SerializeField] private float fadeOutTime;
+    [SerializeField] private float minTimeToShow = 1f;
+    [SerializeField] private Color startTint;
+    [SerializeField] private Color finishTint;
+    [SerializeField] private Image image;
+    [SerializeField] private Animator animator;
     
     private AsyncOperation _currentLoadingOperation;
+    private bool _didTriggerFadeOut;
     private bool _isLoading;
     private Vector3 _barFillLocalScale;
-
+    private float _elapsedTime;
 
     private void Awake()
     {
         if (Instance != null)
         {
-            
             Destroy(gameObject);
             return;
         }
@@ -36,23 +45,53 @@ public class LoadingScreen : MonoBehaviour
         if (!_isLoading) return;
         
         SetProgress(_currentLoadingOperation.progress);
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime >= minTimeToShow)
+            _currentLoadingOperation.allowSceneActivation = true;
 
-        if (_currentLoadingOperation.isDone) 
+        WrapUp();
+    }
+
+    private void WrapUp()
+    {
+        if (!_currentLoadingOperation.isDone || _didTriggerFadeOut) return;
+
+        _didTriggerFadeOut = true;
+        if (animator != null)
+            StartCoroutine(ProcessFadeOut());
+        else
             Hide();
+    }
+
+    private IEnumerator ProcessFadeOut()
+    {
+        animator.SetTrigger("FadeOut");
+        Debug.Log("Triggered");
+        yield return new WaitForSeconds(fadeOutTime);
+        Hide();
+
     }
 
     private void SetProgress(float progress)
     {
-        _barFillLocalScale.x = progress;
-        barFillRectTransform.localScale = _barFillLocalScale;
-        percentLoadedText.text = Mathf.CeilToInt(progress * 100) + "%";
+        var amount = Math.Min(progress, _elapsedTime / minTimeToShow);
+        _barFillLocalScale.x = amount;
+        if (barFillRectTransform != null)
+            barFillRectTransform.localScale = _barFillLocalScale;
+        if (percentLoadedText != null)
+            percentLoadedText.text = Mathf.CeilToInt(amount * 100) + "%";
+        if (image != null)
+            image.color = Color.Lerp(startTint, finishTint, amount);
     }
 
     public void Init(AsyncOperation loadingOperation)
     {
+        _elapsedTime = 0;
         StartCoroutine(BeginShow());
         _currentLoadingOperation = loadingOperation;
+        _currentLoadingOperation.allowSceneActivation = false;
         SetProgress(0f);
+        _didTriggerFadeOut = false;
         _isLoading = true;
     }
 
