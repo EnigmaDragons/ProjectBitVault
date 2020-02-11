3140fa3b06a1ed8fec5d7fae2306e84e70487fa9
noah reinagel <noah.r9@gmail.com>
2020-02-10 18:11:42 -0700

    now removes the hackerselect when starting new game in beta

--- src/BitVault/Assets/Scripts/UI/SetActiveBasedOnBool.cs
+++ src/BitVault/Assets/Scripts/UI/SetActiveBasedOnBool.cs
@@ -3,6 +3,7 @@
 public class SetActiveBasedOnBool : MonoBehaviour
 {
     [SerializeField] private BoolReference boolRef;
+    [SerializeField] private bool _inverted;
 
-    private void Start() => gameObject.SetActive(boolRef.Value);
+    private void Start() => gameObject.SetActive(_inverted ? !boolRef.Value : boolRef.Value);
 }
