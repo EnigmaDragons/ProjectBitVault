5aa3791d39638731f48c12960f15b6ae950c8b78
Silas Reinagel <silas.reinagel@carvana.com>
2020-02-12 17:41:30 -0700

    Added additional Level Complete sound. Closes #589

--- src/BitVault/Assets/Scripts/Sounds/PlayLevelCompleteVictorySound.cs
+++ src/BitVault/Assets/Scripts/Sounds/PlayLevelCompleteVictorySound.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayLevelCompleteVictorySound : OnMessage<LevelCompleted>
 {
     [SerializeField] private UiSfxPlayer player;
     [SerializeField] private AudioClip clip;
-    
-    protected override void Execute(LevelCompleted msg) => player.Play(clip);
+    [SerializeField] private FloatReference delay = new FloatReference(0);
+
+    protected override void Execute(LevelCompleted msg) => StartCoroutine(Play());
+
+    private IEnumerator Play()
+    {
+        yield return new WaitForSeconds(delay);
+        player.Play(clip);
+    }
 }
