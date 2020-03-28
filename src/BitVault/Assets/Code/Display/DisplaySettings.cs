4310eeb1c6a8fc8d10e43c86a42178e59ddf4d6e
SilasReinagel <silas.reinagel@gmail.com>
2020-03-27 20:13:55 -0700

    Bug Fix - Leaves Resolution settings intact when opening Options menu. Closes #685

--- src/BitVault/Assets/Code/Display/DisplaySettings.cs
+++ src/BitVault/Assets/Code/Display/DisplaySettings.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public sealed class DisplaySettings : ScriptableObject
 {
     [SerializeField] private bool isFullscreen = true;
-    [SerializeField] private Resolution resolution;
-
-    private void OnEnable()
-    {
-        resolution = Screen.currentResolution;
-    }
-
+    [SerializeField] private Vector2Int screenSize;
+    
     public bool IsFullscreen => isFullscreen;
+    private string Mode => (isFullscreen ? "Fullscreen" : "Windowed");
+
+    public void InitWithoutChanging() => screenSize = new Vector2Int(Screen.width, Screen.height);
 
     public void SetFullscreen(bool on) => UpdateAfter(() => isFullscreen = on);
     public void ToggleFullscreen() => UpdateAfter(() => isFullscreen = !isFullscreen);
-    public Resolution CurrentResolution => resolution;
-    public void SetResolution(Resolution r) => UpdateAfter(() => resolution = r);
+    public Vector2Int CurrentScreenSize => screenSize;
+    public void SetResolution(Resolution r) => UpdateAfter(() => screenSize = new Vector2Int(r.width, r.height));
     
     private void UpdateAfter(Action set)
     {
-        var old = $"{isFullscreen}-{resolution.width}x{resolution.height}";
+        var old = $"{Mode}-{screenSize.x}x{screenSize.y}";
         set();
-        var newHash = $"{isFullscreen}-{resolution.width}x{resolution.height}";
+        var newHash = $"{Mode}-{screenSize.x}x{screenSize.y}";
         if (newHash != old)
-            Screen.SetResolution(resolution.width, resolution.height, isFullscreen);    
+        {
+            Screen.SetResolution(screenSize.x, screenSize.y, isFullscreen);
+            Debug.Log($"Changed Display/ Old: {old}. New: {newHash}");
+        }
     }
-
 }
