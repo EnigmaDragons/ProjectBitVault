b4703f836070deb3a975c071d6e97ec242fdaedf
SilasReinagel <silas.reinagel@gmail.com>
2020-07-23 23:43:20 -0700

    Auto-Bump Another Attempt

--- src/BitVault/Assets/Scripts/Editor/AutoIncrementVersionCodeInCloudBuild.cs
+++ src/BitVault/Assets/Scripts/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -6,7 +6,7 @@ public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
 #if UNITY_CLOUD_BUILD
     public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
-        string buildNumber = manifest.GetValue<string>("buildNumber", "unknown");
+        string buildNumber = manifest.GetValue<string>("buildNumber");
         Debug.LogWarning("Setting build number to " + buildNumber);
         PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
         PlayerSettings.iOS.buildNumber = buildNumber;
